fd9c02d022e150d8df43bdd3d01a8a7116122b73
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-05-21 15:52:07 +0700

    Kerangka Home

--- application/controllers/Base.php
+++ application/controllers/Base.php
@@ -4,12 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Base extends CI_Controller {
     public function __construct(){
 		parent::__construct();
-        
+
 	}
 
-	public function index()
-	{
-		$this->load->view('welcome_message');
+	public function index(){
+        $data['style'] = $this->load->view('include/ui',NULL, TRUE);
+        $data['nav'] = $this->load->view('components/nav',NULL, TRUE);
+
+		$this->load->view('pages/home', $data);
 	}
 }
 ?>
