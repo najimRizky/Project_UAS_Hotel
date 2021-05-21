5f857d6eb48e0952a6b72c4aee98d85ab1a3fc2b
filbert29 <fkhouwira70@gmail.com>
2021-05-21 15:29:17 +0700

    order anjay

---
+++ application/controllers/Admin.php
@@ -0,0 +1,57 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+        $this->load->library('grocery_CRUD');
+    }
+
+    public function index(){
+        $this->crudItem();
+    }
+
+    public function crudItem(){
+        $crud = new grocery_CRUD();
+        $crud->set_theme('datatables');
+        $crud->set_table('hotel')
+             ->columns('Id_hotel','Nama_hotel','Kota','Lokasi','Bintang','Harga', 'Jumlah_kamar', 'Gambar')
+             ->fields('Id_hotel','Nama_hotel','Kota','Lokasi','Bintang','Harga', 'Jumlah_kamar', 'Gambar')
+             ->callback_edit_field('Lokasi', array($this, 'edit_lokasi'))
+             ->callback_add_field('Lokasi', array($this, 'edit_lokasi'))
+             ->callback_edit_field('Bintang', array($this, 'edit_bintang'))
+             ->callback_edit_field('Id_hotel', array($this, 'edit_id'));
+
+        $output = $crud->render();
+        $data['crud'] = get_object_vars($output);
+        $data['groceryCRUD'] = $this->load->view('include/grocerycrud', $data, TRUE);
+
+        $data['style'] = $this->load->view('include/ui',NULL,TRUE);
+        $this->load->view('admin/hotel', $data);
+    }
+
+    function edit_lokasi($value, $primary_key){
+        return "<textarea name='Deskripsi'> $value </textarea>";
+    }
+
+    function add_lokasi($value, $primary_key){
+        return "<textarea name='Deskripsi'> $value </textarea>";
+    }
+
+    function edit_bintang($value, $primary_key){
+        return '<select name="Bintang">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>';
+    }
+
+    function edit_id($value) {
+        return "<input value='$value' disabled/>";
+    }
+
+
+}
+?>
---
+++ application/views/admin/hotel.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
+    <?= $style ?>
+    <?php echo $groceryCRUD ?>
+
+</head>
+<body>
+    <h1>Hotel</h1>
+    <!-- <a class="ui button" href="<?php //echo base_url('index.php/login/logOut'); ?>">Log Out</a> -->
+    <?php echo $crud['output']; ?>
+</body>
+</html>
