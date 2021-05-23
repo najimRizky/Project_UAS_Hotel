33e73175baa96c0203ec99e785ec37f2451f2730
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-05-23 16:17:09 +0700

    Fix config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Project_UAS_Hotel/';
+$config['base_url'] = 'http://localhost/pemwebuas_lab/';
 
 /*
 |--------------------------------------------------------------------------
