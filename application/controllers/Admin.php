e856144987210da6eff1face2d5e9f060917d640
Bonaventura Sanjaya <bonaventura.sanjaya@student.umn.ac.id>
2021-05-23 14:40:41 +0700

    gajadi db escape, crud admin ubah dikit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -20,9 +20,16 @@ class Admin extends CI_Controller{
              ->fields('Id_hotel','Nama_hotel','Kota','Lokasi','Bintang','Harga', 'Jumlah_kamar', 'Gambar')
              ->unset_clone()
              ->callback_edit_field('Lokasi', array($this, 'edit_lokasi'))
-             ->callback_add_field('Lokasi', array($this, 'edit_lokasi'))
+             ->callback_edit_field('Nama_hotel', array($this, 'edit_nama'))
+             ->callback_edit_field('Kota', array($this, 'edit_kota'))
+             ->callback_edit_field('Gambar', array($this, 'edit_gambar'))
              ->callback_edit_field('Bintang', array($this, 'edit_bintang'))
              ->callback_edit_field('Id_hotel', array($this, 'edit_id'))
+             ->callback_add_field('Lokasi', array($this, 'add_lokasi'))
+             ->callback_add_field('Id_hotel', array($this, 'add_id'))
+             ->callback_add_field('Bintang', array($this, 'add_bintang'))
+             ->set_field_upload('Gambar', 'assets/uploads/hotel')
+
              ->callback_column('Gambar', array($this, 'img_size'));
 
         $output = $crud->render();
@@ -34,10 +41,6 @@ class Admin extends CI_Controller{
     }
 
     function edit_lokasi($value, $primary_key){
-        return "<textarea name='Deskripsi'> $value </textarea>";
-    }
-
-    function add_lokasi($value, $primary_key){
         return "<textarea name='Deskripsi'> $value </textarea>";
     }
 
@@ -53,6 +56,36 @@ class Admin extends CI_Controller{
 
     function edit_id($value) {
         return "<input value='$value' disabled/>";
+    }
+
+    function edit_nama($value) {
+        return "<input value='$value' disabled/>";
+    }
+
+    function edit_kota($value) {
+        return "<input value='$value' disabled/>";
+    }
+
+    function edit_gambar($value) {
+        return "<input value='$value' disabled/>";
+    }
+
+    function add_lokasi($value, $primary_key){
+        return "<textarea name='Deskripsi'> $value </textarea>";
+    }
+
+    function add_id($value, $primary_key){
+        return "<input placeholder='auto-generated' disabled/>";
+    }
+    function add_bintang($value, $primary_key){
+        return '<select name="Bintang">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>';
+        
     }
 
     function img_size($value, $row){
--- application/models/User.php
+++ application/models/User.php
@@ -9,9 +9,7 @@ class User extends CI_Model{
 	}
 
     public function cekUser($email, $password){
-        $val1 = $this->db->escape($email);
-        $val2 = $this->db->escape($password);
-        $query = $this->db->get_where('user', array('Email' => $val1, 'Password' => $val2));
+        $query = $this->db->get_where('user', array('Email' => $email, 'Password' => $password));
         return $query->result_array();
     }
 
